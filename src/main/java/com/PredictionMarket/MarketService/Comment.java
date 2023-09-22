88f2737a7b3b4d55038e32c35c0ef8023f630b2e
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-22 14:18:32 -0700

    Added constructors and additional methods for comment schema

--- src/main/java/com/PredictionMarket/MarketService/Comment.java
+++ src/main/java/com/PredictionMarket/MarketService/Comment.java
@@ -18,5 +18,24 @@ public class Comment {
     @ManyToOne
     private User user;
 
+    Comment() {}
 
+    Comment(String commentContent, Market market, User user) {
+        this.commentContent = commentContent;
+        this.market = market;
+        this.user = user;
+    }
+
+    Comment(String commentContent, Market market) {
+        this.commentContent = commentContent;
+        this.market = market;`
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String newCommentContent) {
+        this.commentContent = newCommentContent;
+    }
 }
--- src/main/java/com/PredictionMarket/MarketService/CommentRepository.java
+++ src/main/java/com/PredictionMarket/MarketService/CommentRepository.java
@@ -2,5 +2,8 @@ package com.PredictionMarket.MarketService;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    public List<Comment> findByMarketId(Long marketId);
 }
