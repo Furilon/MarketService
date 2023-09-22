7e8b3b6128f7d04e3dabe0b8af6007eca3616859
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-21 17:28:00 -0700

    Created a market jpa repository

---
+++ src/main/java/com/PredictionMarket/MarketService/MarketRepository.java
@@ -0,0 +1,5 @@
+package com.PredictionMarket.MarketService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface MarketRepository extends JpaRepository<Market, Integer> {
+}
