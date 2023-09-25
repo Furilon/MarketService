cffdb5d107dffbd81e97cfdbbc7b1d98758e7b8a
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-24 20:14:50 -0700

    JWT filter and service to parse jwt in request

---
+++ src/main/java/com/PredictionMarket/MarketService/config/JwtService.java
@@ -0,0 +1,77 @@
+package com.PredictionMarket.MarketService.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "KpoP+42q33JYp3X3wLS81eLa4fXZFncJbH58PFRUjwXWvcpE631OqFerMCbRdVta/ADMWkQJhPgUpWhKyVG6tg7jfYBqdOHxlXjtkyCg8P7pI2jxBMcW6mF0DLxPr2bO2t9717SMRPZ3bD+/1s1wjDPh9sjCPXQDLcOON+dMctuYte1yhumEFSQhR049fxTQZPp27UUdXDQs5fFqveFJmxCivPkuMCVR3TYDu9kUlpwGL/tsoFO1dNy8ovM7Eyhbi5n0MsssNAjO55gtVKKnSC2/LSZM+r5UOhWKBEmfYVFMxwJROPNjHN6HskQFkRoIwVaFjyS69HPVX/noPuhMfH2aUXsB7QyJK1PxOAHt6+M=";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
